7ded747eea6e41b1bb5ca84e6b4f5f4fed814ed0
dean19900504 <dean19900504@163.com>
2013-04-28 19:41:11 +0800

    Initial Commit

---
+++ TableViewIndex/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TableViewIndex
+//
+//  Created by Dean on 13-4-28.
+//  Copyright (c) 2013年 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TableViewIndex/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableViewIndex
+//
+//  Created by Dean on 13-4-28.
+//  Copyright (c) 2013年 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
